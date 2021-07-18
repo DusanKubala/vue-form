d6232b0fa0bcc222947524aa070e4adca23b7f38
Dusan <dusankubala1996@gmail.com>
2021-07-18 19:34:40 +0200

    Delete skills functionality

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,6 +12,12 @@
         <option value="designer">Web designer</option>
     </select>
 
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <div v-for="skill in skills" :key="skill" class="pill" @click="deleteSkill(skill)">
+        {{ skill }}
+    </div>
+
     <div class="terms">
         <input type="checkbox" required v-model="terms">
         <label>Accept term and conditions</label>
@@ -27,7 +33,25 @@ export default {
             email: '',
             password: '',
             role: 'designer',
-            terms: false
+            terms: false,
+            tempSkill: '',
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if (e.key === ',' && this.skills) {
+                if (!this.skills.includes(this.tempSkill)) {
+                    this.skills.push(this.tempSkill)
+                }
+
+                this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return item !== skill
+            })
         }
     }
 }
@@ -69,5 +93,18 @@ export default {
         margin:  0 10px 0 0;
         position: relative;
         top: 2px;
+    }
+
+    .pill {
+        display: inline-block;
+        margin: 20px 10px 0 0;
+        padding: 6px 12px;
+        background: #eee;
+        border-radius: 20px;
+        font-size: 12px;
+        letter-spacing: 1px;
+        font-weight: bold;
+        color: #777;
+        cursor: pointer;
     }
 </style>
